9c9dd73b59b77c17e09e84b9e70b70902d547530
Alan Roger <AlanRogerMoreiraAragao@proton.me>
2024-05-30 21:20:27 -0300

    redo boilerplate

--- linkedList.scala
+++ linkedList.scala
@@ -1,54 +1,32 @@
 object Script {
   def main(args: Array[String]): Unit = {
-
-
-    class Node(val value: Any) {
-      var next: Node = Node(null) // Point to the empty Node by default
+    def Run(): Unit = {
+        // Main Logic
     }
-    val emptyNode = Node(null)
+    class Node() {
+        // add .next and value arg
+        // handle Node(null) cases
+    }
 
     class LinkedList {
-      var head: Node = emptyNode
-      var tail: Node = emptyNode
+      //add head and tail
 
-      def append(x: Any): Unit = {
-        val item : Node = new Node(x)
-        if (isEmpty) { // Empty list
-          head = item
-          tail = item
-        } else {
-          tail.next = item // Append to the current tail's next
-          tail = item      // Update tail to the newly appended node
-        }
+      def append(value: Any): Unit = {
+        // append logic
       }
 
       def pop(): Any = {
-        if (isEmpty) {
-          throw new NoSuchElementException("Cannot pop from an empty list")
-        } else {
-          var value: Any = head.value
-          if (head == tail) { // Only one element
-            head = emptyNode
-            tail = emptyNode
-          } else {
-            val newHead = head.next
-            head = newHead
-          }
-          value
-        }
+        // pop logic
       }
 
-      def isEmpty: Boolean = head == null
+      def find(value: Any): Node = {
+        //find logic
+      }
 
-      //def search(): Int = {} // Implement search functionality later
-      //def show(): Unit = {}  // Implement display functionality later
+      def show(): Unit = {
+        //show logic
+      }
     }
-
-    val lista = new LinkedList()
-    lista.append(22)
-    lista.append(4)
-
-    println(lista.head) // Output: 22
-    println(lista.tail.value) // Output: 4
+    Run()// Output: 4
   }
 }
